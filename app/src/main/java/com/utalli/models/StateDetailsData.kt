30a8d42da3e261f1d51ec1df4a3c9e8e8cd46c70
Pallavisinha <pallavisinha@virtualemployee.com>
2019-06-07 13:02:34 +0530

    models

---
+++ app/src/main/java/com/utalli/models/StateDetailsData.kt
@@ -0,0 +1,14 @@
+package com.utalli.models
+
+import java.io.Serializable
+
+class StateDetailsData (
+
+    var stateId : String ,
+    var stateName : String,
+    var isSelected : Boolean
+
+): Serializable
+
+
+
