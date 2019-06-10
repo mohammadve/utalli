a60cc0a908eb8749ac8d18aa3f93ddc4c0112eda
favotestx1 <favotestx1@gmail.com>
2019-06-10 12:11:27 +0530

    Updated Code (10_june_2019)

---
+++ app/src/main/java/com/utalli/adapter/PoolListAdapter.kt
@@ -0,0 +1,24 @@
+package com.utalli.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.utalli.R
+
+class PoolListAdapter:RecyclerView.Adapter<PoolListAdapter.PoolListViewHolder>()
+{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PoolListViewHolder {
+        return PoolListViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_pool_list,parent,false))
+    }
+
+    override fun getItemCount(): Int {
+    return 5
+    }
+
+    override fun onBindViewHolder(holder: PoolListViewHolder, position: Int) {
+    }
+
+    inner class PoolListViewHolder(view:View): RecyclerView.ViewHolder(view)
+    {}
+}
