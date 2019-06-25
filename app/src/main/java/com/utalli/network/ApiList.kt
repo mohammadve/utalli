4c8dc3759defcf2a62efbfc681d1deb097e9ddc8
favotestx1 <favotestx1@gmail.com>
2019-06-25 11:43:52 +0530

    Device token update work

--- app/src/main/java/com/utalli/network/ApiList.kt
+++ app/src/main/java/com/utalli/network/ApiList.kt
@@ -3,7 +3,8 @@ package com.utalli.network
 class ApiList {
     companion object {
 
-        val BASE_URL = "http://3.13.3.42:8000/api/v1/"    ///   "http://172.16.200.38:8000/api/v1/"      // liveApi = "http://3.13.3.42:8000/api/v1/"
+        val BASE_URL =
+            "http://3.13.3.42:8000/api/v1/"    ///   "http://172.16.200.38:8000/api/v1/"      // liveApi = "http://3.13.3.42:8000/api/v1/"
         const val SIGNUP_URL = "users/register"
         const val VERIFY_OTP_URL = "users/register"
         const val LOGIN_URL = "users/login"
@@ -30,7 +31,7 @@ class ApiList {
         const val UPDATE_USER_PROFILE_IMAGE = "users/UpdateuserProfileimage"
         const val UPDATE_PROFILE_URL = "users/UpdateuserProfile"
 
-
+        const val UPDATE_DEVICE_TOKEN = "users/updatetoken"
         var ABOUT_US_URL = BASE_URL + "user/about"
         var TERMS_CONDITION_URL = BASE_URL + "user/terms-condition"
 
---
+++ app/src/main/java/com/utalli/viewModels/HomeViewModel.kt
@@ -0,0 +1,50 @@
+package com.utalli.viewModels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.gson.JsonObject
+import com.utalli.R
+import com.utalli.helpers.Utils
+import com.utalli.network.ApiClient
+import com.utalli.network.ApiService
+import retrofit2.Call
+import retrofit2.Response
+
+class HomeViewModel : ViewModel() {
+    var updateTokenResult: MutableLiveData<JsonObject>? = null
+
+    fun updateDeviceToken(authToken: String, userId: String, deviceToken: String): MutableLiveData<JsonObject> {
+        updateTokenResult = MutableLiveData()
+
+
+        var apiService = ApiClient.getClient().create(ApiService::class.java)
+        var call = apiService.updateDeviceToken(authToken, userId.toInt(), deviceToken)
+
+
+        call.enqueue(object : retrofit2.Callback<JsonObject> {
+
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                //Utils.hideProgressDialog()
+                //Utils.showLog(t.message!!)
+            }
+
+            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                //Utils.hideProgressDialog()
+                updateTokenResult!!.value = response.body()
+
+            }
+
+        })
+
+
+
+
+
+
+        return updateTokenResult!!
+
+
+    }
+
+
+}
